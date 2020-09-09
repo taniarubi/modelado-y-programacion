d93fcfe0a294ce53421870d38af50c8d5ca6967d
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:56:04 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConPepperoni.java
+++ practicas/practica2/src/ConPepperoni.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con pepperoni.
  */
 public class ConPepperoni extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
 
-	/**
-	 * Construye una baguette con pepperoni.
-	 * @param baguette la baguette en uso.
-	 */
-	public ConPepperoni(Baguette baguette) {
-		this.baguette = baguette;
-	}
+    /**
+     * Construye una baguette con pepperoni.
+     * @param baguette la baguette en uso.
+     */
+    public ConPepperoni(Baguette baguette) {
+	this.baguette = baguette;
+    }
 
-	/**
-	 * Regresa la descripción de la baguette con pepperoni.
-	 * @return la descripción de la baguette con pepperoni.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Pepperoni $25 \n";
-	}
+    /**
+     * Regresa la descripción de la baguette con pepperoni.
+     * @return la descripción de la baguette con pepperoni.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Pepperoni $25 \n";
+    }
 
-	/**
-	 * Regresa el costo de la baguette con pepperoni.
-	 * @return el costo de la baguette con pepperoni.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 25;
-	}
+    /**
+     * Regresa el costo de la baguette con pepperoni.
+     * @return el costo de la baguette con pepperoni.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 25;
+    }
 }
