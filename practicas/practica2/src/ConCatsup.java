f5654fa79b5570d2462abe1da7762a48d5267c56
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:26:26 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConCatsup.java
+++ practicas/practica2/src/ConCatsup.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con catsup.
  */
 public class ConCatsup extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
+	
+    /**
+     * Construye una baguette con catsup.
+     * @param baguette la baguette en uso.
+     */
+    public ConCatsup(Baguette baguette) {
+        this.baguette = baguette;
+    }
 
-	/**
-	 * Construye una baguette con catsup.
-	 * @param baguette la baguette en uso.
-	 */
-	public ConCatsup(Baguette baguette) {
-		this.baguette = baguette;
-	}
-
-	/**
-	 * Regresa la descripción de la Baguette con catsup.
-	 * @return la descripción de la Baguette con catsup.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Catsup $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la Baguette con catsup.
-	 * @return el costo de la Baguette con catsup.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Regresa la descripción de la Baguette con catsup.
+     * @return la descripción de la Baguette con catsup.
+     */
+    @Override
+    public String getDescripcion() {
+        return baguette.getDescripcion() + "Catsup $10 \n";
+    }
+	
+    /**
+     * Regresa el costo de la Baguette con catsup.
+     * @return el costo de la Baguette con catsup.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 10;
+    }
 }
