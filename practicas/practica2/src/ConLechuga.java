0a2162b4621e7ff397f75c1425779148e2a57b04
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:44:31 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConLechuga.java
+++ practicas/practica2/src/ConLechuga.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con lechuga. 
  */
 public class ConLechuga extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
 
-	/**
-	 * Construye una baguette con lechuga. 
-	 * @param baguette la baguette en uso.
-	 */
-	public ConLechuga(Baguette baguette) {
-		this.baguette = baguette;
-	}
+    /**
+     * Construye una baguette con lechuga. 
+     * @param baguette la baguette en uso.
+     */
+    public ConLechuga(Baguette baguette) {
+	this.baguette = baguette;
+    }
+	
+    /**
+     * Regresa la descripción de la baguette con lechuga.
+     * @return la descripción de la baguette con lechuga.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Lechuga $10 \n";
+    }
 
-	/**
-	 * Regresa la descripción de la baguette con lechuga.
-	 * @return la descripción de la baguette con lechuga.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Lechuga $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con lechuga.
-	 * @return el costo de la baguette con lechuga.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Regresa el costo de la baguette con lechuga.
+     * @return el costo de la baguette con lechuga.
+     */
+    @Override
+    public int getCosto() {
+        return baguette.getCosto() + 10;
+    }
 }
