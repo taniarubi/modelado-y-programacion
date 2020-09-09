2bf5129674a8db001097a4f5a6d442c4aac74311
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:47:27 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConMayonesa.java
+++ practicas/practica2/src/ConMayonesa.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con mayonesa.
  */
 public class ConMayonesa extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
+	
+    /**
+     * Construye una baguette con mayonesa. 
+     * @param baguette la baguette en uso.
+     */
+    public ConMayonesa(Baguette baguette) {
+	this.baguette = baguette;
+    }
 
-	/**
-	 * Construye una baguette con mayonesa. 
-	 * @param baguette la baguette en uso.
-	 */
-	public ConMayonesa(Baguette baguette) {
-		this.baguette = baguette;
-	}
-
-	/**
-	 * Regresa la descripción de la baguette con mayonesa. 
-	 * @return la descripción de la baguette con mayonesa.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Mayonesa $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con mayonesa. 
-	 * @return el costo de la baguette con mayonesa.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Regresa la descripción de la baguette con mayonesa. 
+     * @return la descripción de la baguette con mayonesa.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Mayonesa $10 \n";
+    }
+	
+    /**
+     * Regresa el costo de la baguette con mayonesa. 
+     * @return el costo de la baguette con mayonesa.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 10;
+    }
 }
