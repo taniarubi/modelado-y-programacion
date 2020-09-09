eeb88a9702ac103c9f5a80cec34477d1aebf847b
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:52:48 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConMostaza.java
+++ practicas/practica2/src/ConMostaza.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con mostaza.
  */
 public class ConMostaza extends Baguette {
-	/* La mostaza en uso. */
-	Baguette baguette;
+    /* La mostaza en uso. */
+    Baguette baguette;
+	
+    /**
+     * Construye una baguette con mostaza.
+     * @param baguette la baguette en uso.
+     */
+    public ConMostaza(Baguette baguette) {
+	this.baguette = baguette;
+    }
 
-	/**
-	 * Construye una baguette con mostaza.
-	 * @param baguette la baguette en uso.
-	 */
-	public ConMostaza(Baguette baguette) {
-		this.baguette = baguette;
-	}
+    /**
+     * Regresa la descripción de la baguette con mostaza. 
+     * @return la descripción de la baguette con mostaza.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Mostaza $10 \n";
+    }
 
-	/**
-	 * Regresa la descripción de la baguette con mostaza. 
-	 * @return la descripción de la baguette con mostaza.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Mostaza $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con mostaza. 
-	 * @return el costo de la baguette con mostaza.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Regresa el costo de la baguette con mostaza. 
+     * @return el costo de la baguette con mostaza.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 10;
+    }
 }
