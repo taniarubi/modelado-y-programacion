ec5825fe45b797798b16981508cf79079e614eea
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:35:41 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConJamon.java
+++ practicas/practica2/src/ConJamon.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con jamón.
  */
 public class ConJamon extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
+	
+    /**
+     * Construye una baguette con jamón.
+     * @param baguette la baguette en uso.
+     */
+    public ConJamon(Baguette baguette) {
+	this.baguette = baguette;
+    }
 
-	/**
-	 * Construye una baguette con jamón.
-	 * @param baguette la baguette en uso.
-	 */
-	public ConJamon(Baguette baguette) {
-		this.baguette = baguette;
-	}
-
-	/**
-	 * Regresa la descripción de la baguette con jamón.
-	 * @return la descripción de la baguette con jamón.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Jamón $15 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con jamón. 
-	 * @return el costo de la baguette con jamón.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 15;
-	}
+    /**
+     * Regresa la descripción de la baguette con jamón.
+     * @return la descripción de la baguette con jamón.
+     */
+    @Override
+    public String getDescripcion() {
+        return baguette.getDescripcion() + "Jamón $15 \n";
+    }
+	
+    /**
+     * Regresa el costo de la baguette con jamón. 
+     * @return el costo de la baguette con jamón.
+     */
+    @Override
+    public int getCosto() {
+        return baguette.getCosto() + 15;
+    }
 }
