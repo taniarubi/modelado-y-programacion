45065b726bd037e931e5ee6976c03972b1d628a2
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:39:09 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConJitomate.java
+++ practicas/practica2/src/ConJitomate.java
@@ -2,32 +2,32 @@
  * Clase para una baguette con jitomate. 
  */
 public class ConJitomate extends Baguette {
-	/* La baguette en uso. */
-	Baguette baguette;
+    /* La baguette en uso. */
+    Baguette baguette;
 
-	/**
-	 * Construye una baguette con jitomate.
-	 * @param baguette la baguette en uso.
-	 */
-	public ConJitomate(Baguette baguette) {
-		this.baguette = baguette;
-	}
-
-	/**
-	 * Regresa la descripción de la baguette con jitomate.
-	 * @return la descripción de la baguette con jitomate.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Jitomate $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con jitomate.
-	 * @return el costo de la baguette con jitomate.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Construye una baguette con jitomate.
+     * @param baguette la baguette en uso.
+     */
+    public ConJitomate(Baguette baguette) {
+	this.baguette = baguette;
+    }
+	
+    /**
+     * Regresa la descripción de la baguette con jitomate.
+     * @return la descripción de la baguette con jitomate.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Jitomate $10 \n";
+    }
+	
+    /**
+     * Regresa el costo de la baguette con jitomate.
+     * @return el costo de la baguette con jitomate.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 10;
+    }
 }
