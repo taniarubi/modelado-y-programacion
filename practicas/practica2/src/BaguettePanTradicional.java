482a5e4efb14f0b105cbd00d543104886effdfa8
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:22:26 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/BaguettePanTradicional.java
+++ practicas/practica2/src/BaguettePanTradicional.java
@@ -3,32 +3,32 @@
  * trigo). 
  */
 public class BaguettePanTradicional extends Baguette {
-	/* La descripción de la Baguette. */
-	private String descripcion; 
-	/* El costo del pan tradicional. */
-	private int costo;
+    /* La descripción de la Baguette. */
+    private String descripcion; 
+    /* El costo del pan tradicional. */
+    private int costo;
 
-	/* Construye una baguette con pan tradicional. */
-	public BaguettePanTradicional() {
-		this.descripcion = "Pan tradicional $5 \n";
-		this.costo = 5;
-	}
+    /* Construye una baguette con pan tradicional. */
+    public BaguettePanTradicional() {
+        this.descripcion = "Pan tradicional $5 \n";
+	this.costo = 5;
+    }
+	
+    /**
+     * Regresa la descripción de la Baguette con pan tradicional.
+     * @return la descripción de la Baguette con pan tradicional.
+     */
+    @Override
+    public String getDescripcion() {
+	return descripcion;
+    }
 
-	/**
-	 * Regresa la descripción de la Baguette con pan tradicional.
-	 * @return la descripción de la Baguette con pan tradicional.
-	 */
-	@Override
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	/**
-	 * Regresa el costo de la Baguette con pan tradicional.
-	 * @return el costo de la Baguette con pan tradicional.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de la Baguette con pan tradicional.
+     * @return el costo de la Baguette con pan tradicional.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
