c920006816ac0bf0c3218a0617d6ece40a7cd89f
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:11:11 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/Baguette.java
+++ practicas/practica2/src/Baguette.java
@@ -2,16 +2,16 @@
  * Clase abstracta para el producto Baguette.
  */
 public abstract class Baguette implements Producto {
-	/* La descripción de la Baguette. */
-	private String descripcion;
-	/* El costo total de la Baguette. */
-	private int costo;
-
-	/**
-	 * Regresa la descripción de la baguette.
-	 * @return la descripción de la baguette.
-	 */
-	public String getDescripcion() {
+    /* La descripción de la Baguette. */
+    private String descripcion;
+    /* El costo total de la Baguette. */
+    private int costo;
+	
+    /**
+     * Regresa la descripción de la baguette.
+     * @return la descripción de la baguette.
+     */
+    public String getDescripcion() {
 		return descripcion;
 	}
 
