843fb1e77e100ff035238e5d866f5b0323dc0936
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 17:31:35 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/ConCebolla.java
+++ practicas/practica2/src/ConCebolla.java
@@ -2,33 +2,33 @@
  * Clase para una baguette con cebolla.
  */
 public class ConCebolla extends Baguette {
-	/* La baguette en eso. */
-	Baguette baguette;
+    /* La baguette en eso. */
+    Baguette baguette;
+	
+    /**
+     * Construye una baguette con cebolla. 
+     * @param baguette la baguette en uso.
+     */
+    public ConCebolla(Baguette baguette) {
+        this.baguette = baguette;
+    }
 
-	/**
-	 * Construye una baguette con cebolla. 
-	 * @param baguette la baguette en uso.
-	 */
-	public ConCebolla(Baguette baguette) {
-		this.baguette = baguette;
-	}
+    /**
+     * Regresa la descripción de la baguette con cebolla.
+     * @return la descripción de la baguette con cebolla.
+     */
+    @Override
+    public String getDescripcion() {
+	return baguette.getDescripcion() + "Cebolla $10 \n";
+    }
 
-	/**
-	 * Regresa la descripción de la baguette con cebolla.
-	 * @return la descripción de la baguette con cebolla.
-	 */
-	@Override
-	public String getDescripcion() {
-		return baguette.getDescripcion() + "Cebolla $10 \n";
-	}
-
-	/**
-	 * Regresa el costo de la baguette con cebolla.
-	 * @return el costo de la baguette con cebolla.
-	 */
-	@Override
-	public int getCosto() {
-		return baguette.getCosto() + 10;
-	}
+    /**
+     * Regresa el costo de la baguette con cebolla.
+     * @return el costo de la baguette con cebolla.
+     */
+    @Override
+    public int getCosto() {
+	return baguette.getCosto() + 10;
+    }
 }
 	
