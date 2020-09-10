f80712ccd33fdf540f994af8acfdb6ba5803e2b8
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 11:59:07 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/FactoryProducer.java
+++ practicas/practica3/src/FactoryProducer.java
@@ -2,24 +2,23 @@
  * Clase para fabricar fábricas.
  */
 public class FactoryProducer {
-	
-	/**
-	 * Regresa la fábrica a fabricar.
-	 * @param opcion la opción a fabricar. 
-	 * @return la fábrica a fabricar.
-	 */
-	public static AbstractFactory getFactory(String opcion) {
-		if(opcion.equalsIgnoreCase("llanta")) 
-			return new FabricaLLantas();
-		else if(opcion.equalsIgnoreCase("motor"))
-			return new FabricaMotores();
-		else if(opcion.equalsIgnoreCase("carroceria"))
-			return new FabricaCarrocerias();
-		else if(opcion.equalsIgnoreCase("blindaje"))
-			return new FabricaBlindaje();
-		else if(opcion.equalsIgnoreCase("arma"))
-			return new FabricaArmas();
+    /**
+     * Regresa la fábrica a fabricar.
+     * @param opcion la opción a fabricar. 
+     * @return la fábrica a fabricar.
+     */
+    public static AbstractFactory getFactory(String opcion) {
+        if(opcion.equalsIgnoreCase("llanta")) 
+	    return new FabricaLLantas();
+	else if(opcion.equalsIgnoreCase("motor"))
+	    return new FabricaMotores();
+	else if(opcion.equalsIgnoreCase("carroceria"))
+	    return new FabricaCarrocerias();
+	else if(opcion.equalsIgnoreCase("blindaje"))
+	    return new FabricaBlindaje();
+	else if(opcion.equalsIgnoreCase("arma"))
+	    return new FabricaArmas();
 		
-		return null;
-	}
+	return null;
+    }
 }
