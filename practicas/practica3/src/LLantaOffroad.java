33110cb9755c1a59e842d75dcca7fa89c4d5607f
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 12:09:52 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/LLantaOffroad.java
+++ practicas/practica3/src/LLantaOffroad.java
@@ -2,33 +2,33 @@
  * Clase para las llantas Off-Road.
  */
 public class LLantaOffroad implements LLanta {
-	/* El tipo de llanta. */
-	private String tipo;
-	/* El costo de la llanta. */
-	private int costo;
+    /* El tipo de llanta. */
+    private String tipo;
+    /* El costo de la llanta. */
+    private int costo;
 
-	/* Crea una llanta con tipo y costo. */
-	@Override
-	public void crearLLanta() {
-		this.tipo = "LLanta Off-Road";
-		this.costo = 2000; 
-	}
+    /* Crea una llanta con tipo y costo. */
+    @Override
+    public void crearLLanta() {
+	this.tipo = "LLanta Off-Road";
+	this.costo = 2000; 
+    }
 
-	/**
-	 * Regresa el tipo de llanta.
-	 * @return el tipo de llanta.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el tipo de llanta.
+     * @return el tipo de llanta.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el costo de la llanta.
-	 * @return el costo de la llanta. 
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de la llanta.
+     * @return el costo de la llanta. 
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
