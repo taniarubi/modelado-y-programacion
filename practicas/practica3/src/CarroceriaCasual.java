be1e5e55e61f8d0d0d7e880f4a96083109057b15
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 21:59:20 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/CarroceriaCasual.java
+++ practicas/practica3/src/CarroceriaCasual.java
@@ -2,32 +2,33 @@
  * Clase para las carrocerías casuales.
  */
 public class CarroceriaCasual implements Carroceria {
-	/* El tipo de carrocería. */
-	private String tipo;
-	private int costo;
+    /* El tipo de carrocería. */
+    private String tipo;
+    /* El costo de la carrocería. */
+    private int costo;
 
-	/* Crea una carrocería con tipo y costo. */
-	@Override
-	public void crearCarroceria(){
-		this.tipo = "Carrocería Casual";
-		this.costo = 35000;
-	}
+    /* Crea una carrocería con tipo y costo. */
+    @Override
+    public void crearCarroceria(){
+	this.tipo = "Carrocería Casual";
+	this.costo = 35000;
+    }
+	
+    /**
+     * Regresa el tipo de carrocería.
+     * @return el tipo de carrocería.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el tipo de carrocería.
-	 * @return el tipo de carrocería.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
-
-	/**
-	 * Regresa el costo de carrocería.
-	 * @return el costo de carrocería.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de carrocería.
+     * @return el costo de carrocería.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
