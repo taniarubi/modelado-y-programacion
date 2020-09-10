5231775457384f6a724a3cb3c61ec3d096a3b39d
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 11:47:07 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/FabricaCarrocerias.java
+++ practicas/practica3/src/FabricaCarrocerias.java
@@ -2,32 +2,31 @@
  * Clase para fabricar carrocerías. 
  */
 public class FabricaCarrocerias extends AbstractFactory {
-	
-	/**
-	 * Regresa el componente a fabricar.
-	 * @param tipoComponente el tipo de componente a fabricar.
-	 * @return el componente a fabricar.
-	 */
-	@Override
-	public Object getComponente(String tipoComponente) {
-		return getCarroceria(tipoComponente);
-	}
+    /**
+     * Regresa el componente a fabricar.
+     * @param tipoComponente el tipo de componente a fabricar.
+     * @return el componente a fabricar.
+     */
+    @Override
+    public Object getComponente(String tipoComponente) {
+	return getCarroceria(tipoComponente);
+    }
 
-	/**
-	 * Regresa la carrocería a fabricar.
-	 * @param tipoCarroceria el tipo de carrocería a fabricar.
-	 * @return la carrocería a fabricar.
-	 */
-	public Carroceria getCarroceria(String tipoCarroceria) {
-		if(tipoCarroceria == null)
-			return null;
-		else if(tipoCarroceria.equalsIgnoreCase("casual"))
-			return new CarroceriaCasual();
-		else if(tipoCarroceria.equalsIgnoreCase("camion"))
-			return new CarroceriaDeCamion();
-		else if(tipoCarroceria.equalsIgnoreCase("deportiva"))
-			return new CarroceriaDeportiva();
-
-		return null;
-	}	
+    /**
+     * Regresa la carrocería a fabricar.
+     * @param tipoCarroceria el tipo de carrocería a fabricar.
+     * @return la carrocería a fabricar.
+     */
+    public Carroceria getCarroceria(String tipoCarroceria) {
+        if(tipoCarroceria == null)
+	    return null;
+	else if(tipoCarroceria.equalsIgnoreCase("casual"))
+	    return new CarroceriaCasual();
+	else if(tipoCarroceria.equalsIgnoreCase("camion"))
+	    return new CarroceriaDeCamion();
+	else if(tipoCarroceria.equalsIgnoreCase("deportiva"))
+	    return new CarroceriaDeportiva();
+	    
+	return null;
+    }	
 }
