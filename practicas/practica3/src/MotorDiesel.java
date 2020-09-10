82a87afbac09a01fc071d2921b8aaa18164b4728
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 15:06:54 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/MotorDiesel.java
+++ practicas/practica3/src/MotorDiesel.java
@@ -2,45 +2,45 @@
  * Clase para los motores Diesel.
  */
 public class MotorDiesel implements Motor {
-	/* El tipo de motor. */
-	private String tipo;
-	/* El costo del motor. */
-	private int costo;
-	/* La velocidad del motor. */
-	private int velocidad;
+    /* El tipo de motor. */
+    private String tipo;
+    /* El costo del motor. */
+    private int costo;
+    /* La velocidad del motor. */
+    private int velocidad;
 
-	/* Crea un motor con tipo, costo y velocidad. */
-	@Override
-	public void crearMotor() {
-		this.tipo = "Motor Diesel";
-		this.costo = 100000;
-		this.velocidad = 150;
-	}
+    /* Crea un motor con tipo, costo y velocidad. */
+    @Override
+    public void crearMotor() {
+	this.tipo = "Motor Diesel";
+	this.costo = 100000;
+	this.velocidad = 150;
+    }
 
-	/**
-	 * Regresa el tipo de motor.
-	 * @return el tipo de motor.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el tipo de motor.
+     * @return el tipo de motor.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el costo del motor. 
-	 * @return el costo del motor.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo del motor. 
+     * @return el costo del motor.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 
-	/**
-	 * Regresa la velocidad del motor. 
-	 * @return la velocidad del motor.
-	 */
-	@Override
-	public int getVelocidad() {
-		return velocidad;
-	}
+    /**
+     * Regresa la velocidad del motor. 
+     * @return la velocidad del motor.
+     */
+    @Override
+    public int getVelocidad() {
+	return velocidad;
+    }
 }
