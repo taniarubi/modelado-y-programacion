55c0cc2017b340692d6755ad423d3238f13d4ce4
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:49:51 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/BlindajeDeTanque.java
+++ practicas/practica3/src/BlindajeDeTanque.java
@@ -2,45 +2,45 @@
  * Clase para el blindaje de tanque. 
  */
 public class BlindajeDeTanque implements Blindaje {
-	/* El tipo de blindaje. */
-	private String tipo;
-	/* El costo del blindaje. */
-	private int costo;
-	/* La defensa del blindaje. */
-	private int defensa;
+    /* El tipo de blindaje. */
+    private String tipo;
+    /* El costo del blindaje. */
+    private int costo;
+    /* La defensa del blindaje. */
+    private int defensa;
 
-	/* Crea un blindaje con tipo, costo y defensa. */
-	@Override
-	public void crearBlindaje() {
-		this.tipo = "Blindaje Tanque";
-		this.costo = 36500;
-		this.defensa = 95;
-	}
+    /* Crea un blindaje con tipo, costo y defensa. */
+    @Override
+    public void crearBlindaje() {
+	this.tipo = "Blindaje Tanque";
+	this.costo = 36500;
+	this.defensa = 95;
+    }
+	
+    /**
+     * Regresa el tipo de blindaje.
+     * @return el tipo de blindaje. 
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el tipo de blindaje.
-	 * @return el tipo de blindaje. 
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el costo del blindaje. 
+     * @return el costo del blindaje.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 
-	/**
-	 * Regresa el costo del blindaje. 
-	 * @return el costo del blindaje.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
-
-	/**
-	 * Regresa la defensa del blindaje. 
-	 * @return la defensa del blindaje. 
-	 */
-	@Override
-	public int getDefensa() {
-		return defensa;
-	}
+    /**
+     * Regresa la defensa del blindaje. 
+     * @return la defensa del blindaje. 
+     */
+    @Override
+    public int getDefensa() {
+	return defensa;
+    }
 }
