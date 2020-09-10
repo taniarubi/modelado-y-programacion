b35e3693b20bd1652617dd084f9a89dcea4fda7a
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:11:53 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/Producto.java
+++ practicas/practica2/src/Producto.java
@@ -2,8 +2,8 @@
  * Interfaz para los productos del restaurante. 
  */
 public interface Producto{
-	/* La descripción del producto. */
-	public String getDescripcion();
-	/* El costo total del producto. */
-	public int getCosto();
+    /* La descripción del producto. */
+    public String getDescripcion();
+    /* El costo total del producto. */
+    public int getCosto();
 }
