4b7e73203c9ca4bd28ac4756945b6ed304030400
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 11:49:07 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/FabricaLLantas.java
+++ practicas/practica3/src/FabricaLLantas.java
@@ -2,34 +2,33 @@
  * Clase para fabricar llantas. 
  */
 public class FabricaLLantas extends AbstractFactory {
-	
-	/**
-	 * Regresa el componente a fabricar.
-	 * @param tipoComponente el tipo de componente a fabricar. 
-	 * @return el tipo de componente a fabricar.
-	 */
-	@Override
-	public Object getComponente(String tipoComponente) {
-		return getLLanta(tipoComponente);
-	}
+    /**
+     * Regresa el componente a fabricar.
+     * @param tipoComponente el tipo de componente a fabricar. 
+     * @return el tipo de componente a fabricar.
+     */
+    @Override
+    public Object getComponente(String tipoComponente) {
+	return getLLanta(tipoComponente);
+    }
 
-	/**
-	 * Regresa la llanta a fabricar. 
-	 * @param tipoLLanta el tipo de llanta a fabricar.
-	 * @return la llanta a fabricar.
-	 */
-	public LLanta getLLanta(String tipoLLanta) {
-		if(tipoLLanta == null)
-			return null;
-		else if(tipoLLanta.equalsIgnoreCase("simple"))
-			return new LLantaSimple();
-		else if(tipoLLanta.equalsIgnoreCase("deportiva"))
-			return new LLantaDeportiva();
-		else if(tipoLLanta.equalsIgnoreCase("offroad"))
-			return new LLantaOffroad();
-		else if(tipoLLanta.equalsIgnoreCase("oruga"))
-			return new LLantaOruga();
-			
-		return null;
-	}	
+    /**
+     * Regresa la llanta a fabricar. 
+     * @param tipoLLanta el tipo de llanta a fabricar.
+     * @return la llanta a fabricar.
+     */
+    public LLanta getLLanta(String tipoLLanta) {
+        if(tipoLLanta == null)
+	    return null;
+	else if(tipoLLanta.equalsIgnoreCase("simple"))
+	    return new LLantaSimple();
+	else if(tipoLLanta.equalsIgnoreCase("deportiva"))
+	    return new LLantaDeportiva();
+	else if(tipoLLanta.equalsIgnoreCase("offroad"))
+	    return new LLantaOffroad();
+	else if(tipoLLanta.equalsIgnoreCase("oruga"))
+	    return new LLantaOruga();
+	    
+	return null;
+    }	
 }
