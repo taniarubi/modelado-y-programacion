d67a0159e7800faaddd30459c44c693049aa44a5
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 11:57:39 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/FabricaMotores.java
+++ practicas/practica3/src/FabricaMotores.java
@@ -2,32 +2,31 @@
  * Clase para fabricar motores.
  */
 public class FabricaMotores extends AbstractFactory {
+    /**
+     * Regresa el componente a fabricar.
+     * @param tipoComponente el tipo de componente a fabricar.
+     * @return el componente a fabricar.
+     */
+    @Override
+    public Object getComponente(String tipoComponente) {
+	return getMotor(tipoComponente);
+    }
+
+    /**
+     * Regresa el motor a fabricar.
+     * @param tipoMotor el tipo de motor a fabricar.
+     * @return el motor a fabricar. 
+     */
+    public Motor getMotor(String tipoMotor) {
+        if(tipoMotor == null)
+	    return null;
+	else if(tipoMotor.equalsIgnoreCase("deportivo"))
+	    return new MotorDeportivo();
+	else if(tipoMotor.equalsIgnoreCase("diesel"))
+	    return new MotorDiesel();
+	else if(tipoMotor.equalsIgnoreCase("turbo"))
+	    return new MotorTurbo();
 	
-	/**
-	 * Regresa el componente a fabricar.
-	 * @param tipoComponente el tipo de componente a fabricar.
-	 * @return el componente a fabricar.
-	 */
-	@Override
-	public Object getComponente(String tipoComponente) {
-		return getMotor(tipoComponente);
-	}
-
-	/**
-	 * Regresa el motor a fabricar.
-	 * @param tipoMotor el tipo de motor a fabricar.
-	 * @return el motor a fabricar. 
-	 */
-	public Motor getMotor(String tipoMotor) {
-		if(tipoMotor == null)
-			return null;
-		else if(tipoMotor.equalsIgnoreCase("deportivo"))
-			return new MotorDeportivo();
-		else if(tipoMotor.equalsIgnoreCase("diesel"))
-			return new MotorDiesel();
-		else if(tipoMotor.equalsIgnoreCase("turbo"))
-			return new MotorTurbo();
-
-		return null;
-	}	
+	return null;
+    }	
 }
