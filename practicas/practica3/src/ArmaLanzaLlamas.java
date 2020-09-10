ea714ee3608661f24c0128a1fb4ff518b29f1e5d
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:30:54 -0500

    Modificación de la identación del código.

--- practicas/practica3/src/ArmaLanzaLlamas.java
+++ practicas/practica3/src/ArmaLanzaLlamas.java
@@ -2,45 +2,45 @@
  * Clase para el arma lanza-llamas. 
  */
 public class ArmaLanzaLlamas implements Arma {
-	/* El tipo de arma. */
-	private String tipo;
-	/* El costo del arma. */
-	private int costo;
-	/* El ataque del arma. */
-	private int ataque;
+    /* El tipo de arma. */
+    private String tipo;
+    /* El costo del arma. */
+    private int costo;
+    /* El ataque del arma. */
+    private int ataque;
 
-	/* Crea un arma con tipo, costo y ataque. */
-	@Override
-	public void crearArma() {
-		this.tipo = "Arma Lanzallamas";
-		this.costo = 3500;
-		this.ataque = 65; 
-	}
+    /* Crea un arma con tipo, costo y ataque. */
+    @Override
+    public void crearArma() {
+	this.tipo = "Arma Lanzallamas";
+	this.costo = 3500;
+	this.ataque = 65; 
+    }
+	
+    /**
+     * Regresa el tipo del arma. 
+     * @return el tipo del arma.
+     */
+    @Override
+    public String getTipo(){
+	return tipo;
+    }
 
-	/**
-	 * Regresa el tipo del arma. 
-	 * @return el tipo del arma.
-	 */
-	@Override
-	public String getTipo(){
-		return tipo;
-	}
+    /**
+     * Regresa el costo del arma. 
+     * @return el costo del arma.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 
-	/**
-	 * Regresa el costo del arma. 
-	 * @return el costo del arma.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
-
-	/**
-	 * Regresa el ataque del arma.
-	 * @return el ataque del arma.
-	 */
-	@Override
-	public int getAtaque() {
-		return ataque;
-	}	
+    /**
+     * Regresa el ataque del arma.
+     * @return el ataque del arma.
+     */
+    @Override
+    public int getAtaque() {
+	return ataque;
+    }	
 }
