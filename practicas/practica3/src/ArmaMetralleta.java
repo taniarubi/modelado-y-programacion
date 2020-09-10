e8e94831269e44c7501925b18b5e30ae67780016
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:36:27 -0500

    Modificación de la identación del código.

--- practicas/practica3/src/ArmaMetralleta.java
+++ practicas/practica3/src/ArmaMetralleta.java
@@ -2,45 +2,45 @@
  * Clase para el arma metralleta.
  */
 public class ArmaMetralleta implements Arma {
-	/* El tipo de arma. */
-	private String tipo;
-	/* El costo del arma. */
-	private int costo;
-	/* El ataque del arma. */
-	private int ataque;
+    /* El tipo de arma. */
+    private String tipo;
+    /* El costo del arma. */
+    private int costo;
+    /* El ataque del arma. */
+    private int ataque;
 
-	/* Crea un arma con tipo, costo y ataque. */
-	@Override
-	public void crearArma() {
-		this.tipo = "Arma Metralleta";
-		this.costo = 5000;
-		this.ataque = 100;
-	}
+    /* Crea un arma con tipo, costo y ataque. */
+    @Override
+    public void crearArma() {
+	this.tipo = "Arma Metralleta";
+	this.costo = 5000;
+	this.ataque = 100;
+    }
 
-	/**
-	 * Regresa el tipo de arma. 
-	 * @return el tipo de arma.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el tipo de arma. 
+     * @return el tipo de arma.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el costo del arma. 
-	 * @return el costo del arma.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo del arma. 
+     * @return el costo del arma.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 
-	/**
-	 * Regresa el ataque del arma.
-	 * @return el ataque del arma.
-	 */
-	@Override
-	public int getAtaque() {
-		return ataque;
-	}	
+    /**
+     * Regresa el ataque del arma.
+     * @return el ataque del arma.
+     */
+    @Override
+    public int getAtaque() {
+	return ataque;	
+    }	
 }
