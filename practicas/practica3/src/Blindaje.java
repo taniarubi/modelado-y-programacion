5b68010e4d648fe7fc6c410fb5966f1468733288
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:47:33 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/Blindaje.java
+++ practicas/practica3/src/Blindaje.java
@@ -2,24 +2,24 @@
  * Interfaz para los blindajes.
  */
 public interface Blindaje {
-	/* Crea un blindaje con tipo, costo y defensa. */
-	public void crearBlindaje();
+    /* Crea un blindaje con tipo, costo y defensa. */
+    public void crearBlindaje();
 
-	/**
-	 * Regresa el tipo de blindaje.
-	 * @return el tipo de blindaje.
-	 */
-	public String getTipo();
+    /**
+     * Regresa el tipo de blindaje.
+     * @return el tipo de blindaje.
+     */
+    public String getTipo();
 
-	/**
-	 * Regresa el costo del blindaje.
-	 * @return el costo del blindaje.
-	 */
-	public int getCosto();
+    /**
+     * Regresa el costo del blindaje.
+     * @return el costo del blindaje.
+     */
+    public int getCosto();
 
-	/**
-	 * Regresa la defensa del blindaje.
-	 * @return la defensa del blindaje.
-	 */
-	public int getDefensa();
+    /**
+     * Regresa la defensa del blindaje.
+     * @return la defensa del blindaje.
+     */
+    public int getDefensa();
 }
