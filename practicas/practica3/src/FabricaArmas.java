3c88fee65d57d67c80670b2dde41214955e3ab05
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 22:15:14 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/FabricaArmas.java
+++ practicas/practica3/src/FabricaArmas.java
@@ -2,37 +2,35 @@
  * Clase para fabricar armas. 
  */
 public class FabricaArmas extends AbstractFactory {
-
-	/**
-	 * Regresa el componente a fabricar.
-	 * @param tipoComponente el tipo de componente a fabricar.
-	 * @return el componente a fabricar.
-	 */
-	@Override
-	public Object getComponente(String tipoComponente) {
-		return getArma(tipoComponente);
-	}
-
-	/**
-	 * Regresa el arma a fabricar.
-	 * @param tipoArma el tipo de arma a fabricar.
-	 * @return el arma a fabricar.
-	 */
-	public Arma getArma(String tipoArma) {
-		if(tipoArma == null)
-			return null;
-		else if(tipoArma.equalsIgnoreCase("arpones"))
-			return new ArmaArpon();
-		else if(tipoArma.equalsIgnoreCase("lanza llamas"))
-			return new ArmaLanzaLlamas();
-		else if(tipoArma.equalsIgnoreCase("cañones"))
-			return new ArmaCanones();
-		else if(tipoArma.equalsIgnoreCase("sierra"))
-			return new ArmaSierra();
-		else if(tipoArma.equalsIgnoreCase("metralleta"))
-			return new ArmaMetralleta();
-		
-		return null;
-	}
-
+    /**
+     * Regresa el componente a fabricar.
+     * @param tipoComponente el tipo de componente a fabricar.
+     * @return el componente a fabricar.
+     */
+    @Override
+    public Object getComponente(String tipoComponente) {
+	return getArma(tipoComponente);
+    }
+	
+    /**
+     * Regresa el arma a fabricar.
+     * @param tipoArma el tipo de arma a fabricar.
+     * @return el arma a fabricar.
+     */
+    public Arma getArma(String tipoArma) {
+        if(tipoArma == null)
+	    return null;
+	else if(tipoArma.equalsIgnoreCase("arpones"))
+	    return new ArmaArpon();
+	else if(tipoArma.equalsIgnoreCase("lanza llamas"))
+	    return new ArmaLanzaLlamas();
+	else if(tipoArma.equalsIgnoreCase("cañones"))
+	    return new ArmaCanones();
+	else if(tipoArma.equalsIgnoreCase("sierra"))
+	    return new ArmaSierra();
+	else if(tipoArma.equalsIgnoreCase("metralleta"))
+	    return new ArmaMetralleta();
+	    
+	return null;
+    }
 }
