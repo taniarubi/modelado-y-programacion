f42419a5fe7d08a3ada412e7dc6aba858bdc2d94
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 12:11:32 -0500

    Update LLantaOruga.java

--- practicas/practica3/src/LLantaOruga.java
+++ practicas/practica3/src/LLantaOruga.java
@@ -2,33 +2,33 @@
  * Clase para las llantas Oruga de Tanque.
  */
 public class LLantaOruga implements LLanta {
-	/* El tipo de llanta. */
-	String tipo;
-	/* El costo de la llanta. */
-	int costo;
+    /* El tipo de llanta. */
+    private String tipo;
+    /* El costo de la llanta. */
+    private int costo;
 
-	/* Crea una llanta con tipo y costo. */
-	@Override
-	public void crearLLanta() {
-		this.tipo = "Llanta Oruga de Tanque";
-		this.costo = 2500;
-	}
+    /* Crea una llanta con tipo y costo. */
+    @Override
+    public void crearLLanta() {
+	this.tipo = "Llanta Oruga de Tanque";
+	this.costo = 2500;
+    }
 
-	/**
-	 * Regresa el tipo de llanta. 
-	 * @return el tipo de llanta.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el tipo de llanta. 
+     * @return el tipo de llanta.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el costo de la llanta.
-	 * @return el costo de la llanta.
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de la llanta.
+     * @return el costo de la llanta.
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
