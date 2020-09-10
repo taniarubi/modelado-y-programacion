8f58319090db64c8c1a0161e10c0cc26cfed8f13
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-10 12:12:55 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/LLantaSimple.java
+++ practicas/practica3/src/LLantaSimple.java
@@ -2,33 +2,33 @@
  * Clase para las llantas simples.
  */
 public class LLantaSimple implements LLanta {
-	/* El tipo de llanta. */
-	private String tipo;
-	/* El costo de la llanta. */
-	private int costo;
+    /* El tipo de llanta. */
+    private String tipo;
+    /* El costo de la llanta. */
+    private int costo;
 
-	/* Crea una llanta con tipo y costo. */
-	@Override
-	public void crearLLanta() {
-		this.tipo = "Llanta Simple";
-		this.costo = 600;
-	}
+    /* Crea una llanta con tipo y costo. */
+    @Override
+    public void crearLLanta() {
+	this.tipo = "Llanta Simple";
+	this.costo = 600;
+    }
 
-	/**
-	 * Regresa el tipo de llanta. 
-	 * @return el tipo de llanta.
-	 */
-	@Override
-	public String getTipo() {
-		return tipo;
-	}
+    /**
+     * Regresa el tipo de llanta. 
+     * @return el tipo de llanta.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el costo de la llanta.
-	 * @return el costo de la llanta. 
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de la llanta.
+     * @return el costo de la llanta. 
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
