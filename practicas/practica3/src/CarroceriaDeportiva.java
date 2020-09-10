e8c0ac5550a21e6b52ce73f6f5834ce6152c1321
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 22:04:12 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/CarroceriaDeportiva.java
+++ practicas/practica3/src/CarroceriaDeportiva.java
@@ -2,33 +2,33 @@
  * Clase para las carrocerías deportivas. 
  */
 public class CarroceriaDeportiva implements Carroceria {
-	/* El tipo de carrocería. */
-	private String tipo;
-	/* El costo de la carrocería. */
-	private int costo;
+    /* El tipo de carrocería. */
+    private String tipo;
+    /* El costo de la carrocería. */
+    private int costo;
 
-	/* Crea una carrocería con tipo y costo. */
-	@Override
-	public void crearCarroceria() {
-		this.tipo = "Carroceria Deportiva";
-		this.costo = 55000;
-	}
+    /* Crea una carrocería con tipo y costo. */
+    @Override
+    public void crearCarroceria() {
+	this.tipo = "Carroceria Deportiva";
+	this.costo = 55000;
+    }
+	
+    /**
+     * Regresa el tipo de carrocería. 
+     * @return el tipo de carrocaería.
+     */
+    @Override
+    public String getTipo() {
+	return tipo;
+    }
 
-	/**
-	 * Regresa el tipo de carrocería. 
-	 * @return el tipo de carrocaería.
-	 */
-	@Override
-	public String getTipo(){
-		return tipo;
-	}
-
-	/**
-	 * Regresa el costo de la carrocería. 
-	 * @return el costo de la carrocería. 
-	 */
-	@Override
-	public int getCosto() {
-		return costo;
-	}
+    /**
+     * Regresa el costo de la carrocería. 
+     * @return el costo de la carrocería. 
+     */
+    @Override
+    public int getCosto() {
+	return costo;
+    }
 }
