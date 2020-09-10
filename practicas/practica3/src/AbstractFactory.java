82ef0ec200aa5c8da555db1fca6c7eff77b8ec1c
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:13:49 -0500

    Modificación de la identación del código.

--- practicas/practica3/src/AbstractFactory.java
+++ practicas/practica3/src/AbstractFactory.java
@@ -2,9 +2,9 @@
  * Clase abstracta que nos permite tratar a todas las fábricas como iguales.
  */
 public abstract class AbstractFactory {
-	/**
-	 * Regresa el componente a fabricar. 
-	 * @param tipoComponente el tipo de componente a fabricar. 
-	 */
-	abstract Object getComponente(String tipoComponente);
+    /**
+     * Regresa el componente a fabricar. 
+     * @param tipoComponente el tipo de componente a fabricar. 
+     */
+    abstract Object getComponente(String tipoComponente);
 }
