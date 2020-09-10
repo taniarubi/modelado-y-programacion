a435f96574f4af1d21727c1704e275bb4f149f24
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:19:14 -0500

    Modificación de la identación del código.

--- practicas/practica3/src/Arma.java
+++ practicas/practica3/src/Arma.java
@@ -2,24 +2,24 @@
  * Interfaz para las armas. 
  */
 public interface Arma {
-	/* Crea un arma con tipo, costo y ataque. */
-	public void crearArma();
+    /* Crea un arma con tipo, costo y ataque. */
+    public void crearArma();
 
-	/**
-	 * Regresa el tipo de arma. 
-	 * @return el tipo de arma.
-	 */
-	public String getTipo();
-
-	/**
-	 * Regresa el costo del arma.
-	 * @return el costo del arma.
-	 */
-	public int getCosto();
-
-	/**
-	 * Regresa el ataque del arma. 
-	 * @return el ataque del arma.
-	 */
-	public int getAtaque();
+    /**
+     * Regresa el tipo de arma. 
+     * @return el tipo de arma.
+     */
+    public String getTipo();
+	
+    /**
+     * Regresa el costo del arma.
+     * @return el costo del arma.
+     */
+    public int getCosto();
+	
+    /**
+     * Regresa el ataque del arma. 
+     * @return el ataque del arma.
+     */
+    public int getAtaque();
 }
