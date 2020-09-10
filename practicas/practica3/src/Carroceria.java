b10a9896c0006c3962401f802bff362338e8ff78
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 21:56:37 -0500

    Modificación en la identación del código.

--- practicas/practica3/src/Carroceria.java
+++ practicas/practica3/src/Carroceria.java
@@ -2,18 +2,18 @@
  * Interfaz para la carrocería.
  */
 public interface Carroceria {
-	/* Crea una carrocería con tipo y costo. */
-	public void crearCarroceria();
+    /* Crea una carrocería con tipo y costo. */
+    public void crearCarroceria();
 
-	/**
-	 * Regresa el tipo de carrocería.
-	 * @return el tipo de carrocería.
-	 */
-	public String getTipo();
-
-	/**
-	 * Regresa el costo de la carrocería.
-	 * @return el costo de la carrocería.
-	 */
-	public int getCosto();
+    /**
+     * Regresa el tipo de carrocería.
+     * @return el tipo de carrocería.
+     */
+    public String getTipo();
+	
+    /**
+     * Regresa el costo de la carrocería.
+     * @return el costo de la carrocería.
+     */
+    public int getCosto();
 }
