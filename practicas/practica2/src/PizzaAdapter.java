d22a661296804e311e02194425c1f77171b4b879
Tania Rubí <30444771+taniarubi@users.noreply.github.com>
2020-09-09 20:10:40 -0500

    Modificación de la identación del código.

--- practicas/practica2/src/PizzaAdapter.java
+++ practicas/practica2/src/PizzaAdapter.java
@@ -2,32 +2,32 @@
  * Clase para adaptar el producto Pizzza.
  */
 public class PizzaAdapter implements Producto {
-	/* La pizza que vamos a adaptar. */
-	private Pizza pizza;
+    /* La pizza que vamos a adaptar. */
+    private Pizza pizza;
 
-	/**
-	 * Construye una pizza adaptada. 
-	 * @param pizza la pizza que vamos a adaptar.
-	 */
-	public PizzaAdapter(Pizza pizza) {
-		this.pizza = pizza;
-	}
+    /**
+     * Construye una pizza adaptada. 
+     * @param pizza la pizza que vamos a adaptar.
+     */
+    public PizzaAdapter(Pizza pizza) {
+	this.pizza = pizza;
+    }
 
-	/**
-	 * Regresa la descripción de la pizza. 
-	 * @return la descripción de la pizza.
-	 */
-	@Override
-	public String getDescripcion() {
-		return pizza.getTipoQueso() + "\n" + pizza.getTipoCarne() + "\n" + pizza.getTipoMasa() + "\n";
-	}
+    /**
+     * Regresa la descripción de la pizza. 
+     * @return la descripción de la pizza.
+     */
+    @Override
+    public String getDescripcion() {
+	return pizza.getTipoQueso() + "\n" + pizza.getTipoCarne() + "\n" + pizza.getTipoMasa() + "\n";
+    }
 	
-	/**
-	 * Regresa el costo de la pizza.
-	 * @return el costo de la pizza.s
-	 */
-	@Override
-	public int getCosto() {
-		return pizza.getCosto();
-	}
+    /**
+     * Regresa el costo de la pizza.
+     * @return el costo de la pizza.
+     */
+    @Override
+    public int getCosto() {
+	return pizza.getCosto();
+    }
 }
